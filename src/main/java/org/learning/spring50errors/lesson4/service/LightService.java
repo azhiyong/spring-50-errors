8c56aa6f90bf591c27240ab8ae5d2a2140fd6377
lizhiyong <lizhiyong@kuaishou.com>
2023-03-21 16:21:37 +0800

    update lesson1~4

--- src/main/java/org/learning/spring50errors/lesson4/service/LightService.java
+++ src/main/java/org/learning/spring50errors/lesson4/service/LightService.java
@@ -1,16 +1,18 @@
 package org.learning.spring50errors.lesson4.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class LightService {
 
     public void check() {
-        System.out.println("light service check");
+        log.info("light service check");
     }
 
     public void shutdown() {
-        System.out.println("light service shutdown");
+        log.info("light service shutdown");
     }
 
 //    @Service和@Bean注解在shutdown方法上不同
